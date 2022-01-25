583e5baf6cd83545238df845c8962e6edcbe640e
YelovSK <hampatrik@gmail.com>
2022-01-25 14:29:32 +0100

    fix average slider

--- Assets/Scripts/WebcamInput.cs
+++ Assets/Scripts/WebcamInput.cs
@@ -33,6 +33,8 @@ public sealed class WebcamInput : MonoBehaviour
 
     void Update()
     {
+        if (_webcam == null)
+            return;
         if (!_webcam.didUpdateThisFrame) return;
 
         var aspect1 = (float)_webcam.width / _webcam.height;
