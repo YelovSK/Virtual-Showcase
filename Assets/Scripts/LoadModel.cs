1e52f762ecc79075a6ed22ab0b246abce94f9742
YelovSK <hampatrik@gmail.com>
2022-01-30 03:16:14 +0100

    control model with mouse

--- Assets/Scripts/LoadModel.cs
+++ Assets/Scripts/LoadModel.cs
@@ -6,70 +6,47 @@ using Dummiesman;
 public class LoadModel : MonoBehaviour
 {
     GameObject loadedObject;
-    float angle = 0f;
 
     void Start()
     {
         if (loadedObject != null)
-        {
             Destroy(loadedObject);
-        }
         Debug.Log(GlobalVars.modelPath);
         loadedObject = new OBJLoader().Load(GlobalVars.modelPath);
-        loadedObject.transform.localPosition = new Vector3(0, 0, 0);
+        loadedObject.transform.Translate(0, 0, -7);
+        loadedObject.transform.Rotate(0, 180, 0);
     }
 
     void Update()
     {
-        float moveBy = 10f*Time.deltaTime;
-        if (Input.GetKey(KeyCode.UpArrow))
+        var mouseX = Input.GetAxis("Mouse X");
+        var mouseY = Input.GetAxis("Mouse Y");
+        if (Input.GetMouseButton(0) && Input.GetMouseButton(1))
         {
-            loadedObject.transform.localPosition += new Vector3(0, moveBy, 0);
+            loadedObject.transform.Translate(0, mouseY/20, 0);
+            return;
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetMouseButton(0))
         {
-            loadedObject.transform.localPosition += new Vector3(0, -moveBy, 0);
+            loadedObject.transform.Rotate(0, -mouseX, 0);
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetMouseButton(1))
         {
-            angle += 1f;
-            loadedObject.transform.localRotation = Quaternion.Euler(0f, angle, 0f);
+            loadedObject.transform.Translate(mouseX/20, 0, mouseY/20, Space.World);
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
         {
-            angle -= 1f;
-            loadedObject.transform.localRotation = Quaternion.Euler(0f, angle, 0f);
+            loadedObject.transform.localScale *= 1.1f;
         }
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
         {
-            loadedObject.transform.localPosition += new Vector3(-moveBy, 0, 0);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            loadedObject.transform.localPosition += new Vector3(+moveBy, 0, 0);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            loadedObject.transform.localPosition += new Vector3(0, 0, -moveBy);
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            loadedObject.transform.localPosition += new Vector3(0, 0, moveBy);
-        }
-        if (Input.GetKey(KeyCode.E))
-        {
-            loadedObject.transform.localScale *= 1.01f;
-        }
-        if (Input.GetKey(KeyCode.Q))
-        {
-            loadedObject.transform.localScale *= 0.99f;
+            loadedObject.transform.localScale *= 0.9f;
         }
         if (Input.GetKeyDown(KeyCode.R))
         {
             loadedObject.transform.localScale = new Vector3(1f, 1f, 1f);
-            loadedObject.transform.localPosition = new Vector3(0f, 0f, 0f);
-            loadedObject.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
-            angle = 0f;
+            loadedObject.transform.localPosition = new Vector3(0f, 0f, -7f);
+            loadedObject.transform.localRotation = Quaternion.Euler(0f, 180f, 0f);
         }
     }
 
