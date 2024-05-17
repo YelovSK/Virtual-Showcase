d410509a600399ce906e17128448e4808bb14a23
YelovSK <hampatrik@gmail.com>
2024-05-17 15:21:30 +0200

    Remove prints

--- scripts/align/registration.py
+++ scripts/align/registration.py
@@ -30,21 +30,18 @@ class GlobalRegistration(Registration):
                 o3d.pipelines.registration.CorrespondenceCheckerBasedOnEdgeLength(0.1),
                 o3d.pipelines.registration.CorrespondenceCheckerBasedOnDistance(distance_threshold)
             ],
-            o3d.pipelines.registration.RANSACConvergenceCriteria(100000, 0.999)
+            o3d.pipelines.registration.RANSACConvergenceCriteria(100_000, 0.999)
         )
 
         return result
 
     def _preprocess_point_cloud(self, pcd):
-        print(":: Downsample with a voxel size %.3f." % self.voxel_size)
         pcd_down = pcd.voxel_down_sample(self.voxel_size)
 
         radius_normal = self.voxel_size * 2
-        print(":: Estimate normal with search radius %.3f." % radius_normal)
         pcd_down.estimate_normals(o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=30))
 
         radius_feature = self.voxel_size * 5
-        print(":: Compute FPFH feature with search radius %.3f." % radius_feature)
         pcd_fpfh = o3d.pipelines.registration.compute_fpfh_feature(pcd_down, o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100))
 
         return pcd_down, pcd_fpfh
